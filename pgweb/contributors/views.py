390e1ca5b5e8d7485d9e396cb7dc435b4a6da7b6
Melanie Plageman <melanieplageman@gmail.com>
2025-11-14 17:04:26 -0500

    contributor profiles

--- pgweb/contributors/views.py
+++ pgweb/contributors/views.py
@@ -1,10 +1,26 @@
+from django.shortcuts import get_object_or_404
 from pgweb.util.contexts import render_pgweb
 
-from .models import ContributorType
+from .models import ContributorType, Contributor, ContributorBadgeAward
 
 
 def completelist(request):
     contributortypes = list(ContributorType.objects.all())
     return render_pgweb(request, 'community', 'contributors/list.html', {
         'contributortypes': contributortypes,
+    })
+
+
+def profile(request, contributor_id):
+    contributor = get_object_or_404(Contributor, pk=contributor_id)
+
+    # Get all active badge awards for this contributor
+    badge_awards = ContributorBadgeAward.objects.filter(
+        contributor=contributor,
+        active=True
+    ).select_related('badge', 'badge__org').order_by('-awarded_date')
+
+    return render_pgweb(request, 'community', 'contributors/profile.html', {
+        'contributor': contributor,
+        'badge_awards': badge_awards,
     })
